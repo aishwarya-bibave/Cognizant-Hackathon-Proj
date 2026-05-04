bba8911b26fbea58f9bc34e9f04307c8fe480308
Slipstream <hatkardhruv@gmail.com>
2026-05-04 12:30:34 +0530

    fix: resolved broken imports after package name change

--- src/test/java/org/booking/testcases/sasikanta/TC_09_ValidAdultCount.java
+++ src/test/java/org/booking/testcases/sasikanta/TC_09_ValidAdultCount.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import org.zigwheels.pages.HomePage;
+import org.booking.pages.HomePage;
 
 public class TC_09_ValidAdultCount extends BaseTest {
 
